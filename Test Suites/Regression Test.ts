0ff0ac2c91e07764742b49998b20c858cd9991af
wildan <wildanbaihaqi01@gmail.com>
2023-01-13 15:42:41 +0700

    Pertama

--- Test Suites/Regression Test.ts
+++ Test Suites/Regression Test.ts
@@ -12,31 +12,31 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>1757c539-a740-4243-8f0d-139596dd7e83</testSuiteGuid>
    <testCaseLink>
-      <guid>f3e39acd-f50d-40b9-9bdc-1e913233dfef</guid>
+      <guid>a041ea56-15b6-44cb-9d22-30950ada1562</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC01-Validation Response And Verify JSON Value</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e0450305-5db3-4f6d-b6fa-0f1c42e21499</guid>
+      <guid>8151f152-07c7-4378-822d-5f7ac374132b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC02-Validation Response</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bfdee0ce-2f75-4686-be50-24aa99a62e5b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC04-Validation Response Create User</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d5653f87-cce4-44b7-b951-b38174b92430</guid>
+      <guid>8490b018-23e3-426c-9c3e-dbb00cd45840</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC03-Validation Response Update User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0e2ccc60-eab4-47e7-bf90-1c0f23badebf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04-Validation Response Create User</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
